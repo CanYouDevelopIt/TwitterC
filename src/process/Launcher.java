5e1774610c7347b628caab49c699e0c6e5c88ec5
jkn1092 <jkn1092@gmail.com>
2015-10-11 16:12:54 +0200

    commit

--- src/process/Launcher.java
+++ src/process/Launcher.java
@@ -1,12 +1,17 @@
 package process;
 
 import javax.swing.JOptionPane;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import twitter.client.TwitterW;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
-		
+	
         String pin = (String) JOptionPane.showInputDialog(
                null,
                "<html>Saisir code <b>pin</b> : </html>",
@@ -15,11 +20,26 @@ public class Launcher {
                null,
                null,
                "");
+       
+        if(pin.equals("634219313") && OAuth()){
+        	new TwitterW();
+        }
+	}
+	
+	public static boolean OAuth(){
 		
-        
-        
-		new TwitterW();
+		try {
+			HttpResponse<String> response = Unirest
+					.get("http://localhost:8080/TwitterCRest/twitterc/usertimeline/auth")
+					.header("", "text/plain").asString();
+			
+			return response.getBody().equals("true");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
+		return false;
 	}
 	
 }
